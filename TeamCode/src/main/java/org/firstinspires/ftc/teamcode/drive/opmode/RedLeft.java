f5a524c897d2660d0818f2b712cecd7e07b4522f
Venkat Hari <venkatmail@gmail.com>
2021-10-17 10:58:45 -0400

    Changed constants after PID tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/RedLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/RedLeft.java
@@ -16,11 +16,7 @@ public class RedLeft extends LinearOpMode {
 
     public static Pose2d startingPosition = new Pose2d(-30, -63, Math.toRadians(270));
 
-    public static Vector2d shippingHubVector = new Vector2d(-13, -40);
-    public static Pose2d shippingHubPose = new Pose2d(shippingHubVector, Math.toRadians(270));
 
-    public static Vector2d carousalVector = new Vector2d(-56, -57);
-    public static Pose2d carousalPose = new Pose2d(carousalVector, Math.toRadians(225));
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -36,25 +32,52 @@ public class RedLeft extends LinearOpMode {
 
         //Step-2 : Drive to Team Shipping Hub (-13, -40) in a reversed direction : end tangent is 270
 
-        Trajectory trajectoryToShippingHub = driveTrain.trajectoryBuilder(startingPosition, true)
-                .splineTo(shippingHubVector, Math.toRadians(270))
+       /* Trajectory trajectoryToShippingHub = driveTrain.trajectoryBuilder(startingPosition, true)
+                .splineTo(shippingHubVector, Math.toRadians(225))
                 .build();
-        driveTrain.followTrajectory(trajectoryToShippingHub);
+        driveTrain.followTrajectory(trajectoryToShippingHub);*/
+
+        Trajectory strafeLeft = driveTrain.trajectoryBuilder(startingPosition, false)
+                .strafeLeft(33)
+                .build();
+        driveTrain.followTrajectory(strafeLeft);
+
+        Trajectory pathToShippingHub = driveTrain.trajectoryBuilder(strafeLeft.end(), false)
+                .back(24)
+                .build();
+
+        driveTrain.followTrajectory(pathToShippingHub);
+        sleep(2000);
+        Trajectory forwardPath = driveTrain.trajectoryBuilder(pathToShippingHub.end(), false)
+                .forward(20)
+                .build();
+        driveTrain.followTrajectory(forwardPath);
+
+        Trajectory strafeRight = driveTrain.trajectoryBuilder(forwardPath.end(),false)
+                .strafeRight(55)
+                .build();
+        driveTrain.followTrajectory(strafeRight);
+        sleep(2000);
+        Trajectory reversePath = driveTrain.trajectoryBuilder(strafeRight.end(),false)
+                .back(26)
+                .build();
+        driveTrain.followTrajectory(reversePath);
 
         //Step-3 : Drop the pre-loaded box in the appropriate level
 
         //Step-4 : Drive to Carousal (-56, -57) with end tangent of 225
-        Trajectory trajectoryToCarousal = driveTrain.trajectoryBuilder(shippingHubPose, false)
-                .splineTo(carousalVector, Math.toRadians(225))
+        //driveTrain.setPoseEstimate(trajectoryToShippingHub.end());
+       /* Trajectory trajectoryToCarousal = driveTrain.trajectoryBuilder(trajectoryToShippingHub.end(), false)
+                .forward(24).strafeRight(55)
                 .build();
         driveTrain.followTrajectory(trajectoryToCarousal);
-
+        sleep(2000);*/
         //Step-5 : Spin the Carousal
 
         //Step 6 : Drive to Storage Unit (-61, -36) with end tangent of 0
-        Trajectory trajectoryToStorageUnit = driveTrain.trajectoryBuilder(carousalPose, false)
-                .strafeRight(24)
+        /*Trajectory trajectoryToStorageUnit = driveTrain.trajectoryBuilder(trajectoryToCarousal.end(), false)
+                .back(24)
                 .build();
-        driveTrain.followTrajectory(trajectoryToStorageUnit);
+        driveTrain.followTrajectory(trajectoryToStorageUnit);*/
     }
 }
