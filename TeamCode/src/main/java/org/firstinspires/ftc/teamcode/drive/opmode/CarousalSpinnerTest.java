0ff86844414a1b1a4b775aa4bb8fbf6030addd94
Venkat Hari <venkatmail@gmail.com>
2021-10-24 13:39:50 -0400

    changed spinner to DC from Servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/CarousalSpinnerTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/CarousalSpinnerTest.java
@@ -5,27 +5,19 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.teamcode.drive.CarousalSpinner;
+
 @Autonomous
 public class CarousalSpinnerTest extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
 
-        //Spinner servo is on control hub -- port 5
-
-        Servo spinner = hardwareMap.get(Servo.class, "spinner");
-        spinner.setDirection(Servo.Direction.REVERSE);
+        CarousalSpinner spinner = new CarousalSpinner(hardwareMap);
 
         waitForStart();
 
-        long start = System.currentTimeMillis();
-
-        //Spin for 5 seconds
-        long end = start + 5*1000;
-
-        while (System.currentTimeMillis() < end) {
-            spinner.setPosition(1.0);
-        }
+        spinner.spin();
 
     }
 }
