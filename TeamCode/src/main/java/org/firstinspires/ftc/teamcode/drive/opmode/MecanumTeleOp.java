a70a8246681bb591d45329c7ddf55d6c40130899
Vishal <vishallokesh09@gmail.com>
2021-10-29 20:09:15 -0400

    updated TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/MecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/MecanumTeleOp.java
@@ -50,9 +50,9 @@ public class MecanumTeleOp extends OpMode {
     @Override
     public void loop() {
 
-        double y = -gamepad1.left_stick_y; // Remember, this is reversed!
-        double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
-        double rx = gamepad1.right_stick_x;
+        double y = -gamepad1.right_stick_y; // Remember, this is reversed!
+        double x = gamepad1.right_stick_x * 1.1; // Counteract imperfect strafing
+        double rx = gamepad1.left_stick_x;
 
         // Denominator is the largest motor power (absolute value) or 1
         // This ensures all the powers maintain the same ratio, but only when
