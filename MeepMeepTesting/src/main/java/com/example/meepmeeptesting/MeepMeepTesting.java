cd2d40ae6603e5f0920225c2f4db2d6a669d5ef4
Venkat Hari <venkatmail@gmail.com>
2021-11-08 17:02:08 -0500

    more options to meep meep

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -2,6 +2,7 @@ package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.MarkerCallback;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
@@ -9,6 +10,13 @@ import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
 import java.util.Vector;
 
 public class MeepMeepTesting {
+
+    public static double MAX_VEL = 42;
+    public static double MAX_ACCEL = 30;
+    public static double MAX_ANG_VEL = Math.toRadians(60);
+    public static double MAX_ANG_ACCEL = Math.toRadians(60);
+
+
     public static void main(String[] args) {
         // TODO: If you experience poor performance, enable this flag
         // System.setProperty("sun.java2d.opengl", "true");
@@ -23,11 +31,15 @@ public class MeepMeepTesting {
                 // Background opacity from 0-1
                 .setBackgroundAlpha(1f)
                 // Set constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                //.setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .setConstraints(MAX_VEL, MAX_ACCEL, MAX_ANG_VEL, MAX_ANG_ACCEL, 9)
                 .followTrajectorySequence(drive ->
                         drive.trajectorySequenceBuilder(new Pose2d(-36, -60, Math.toRadians(270)))
-                                .splineTo(new Vector2d(-20, -20), Math.toRadians(180))
+                                //.splineTo(new Vector2d(-20, -20), Math.toRadians(180))
                                 //.back(42)
+                                //.turn(Math.toRadians(-90))
+                                //.back(10)
+                                //.strafeTo(new Vector2d(-16, -30))
+                                .lineToLinearHeading(new Pose2d(-16, -30, Math.toRadians(270)))
                                 //.turn(Math.toRadians(-90))
                                 //.back(10)
 //                                .turn(Math.toRadians(90))
